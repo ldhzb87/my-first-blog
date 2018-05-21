da83c0f9df3f6663d494702d1ce2d87e7292d345
ldh <ldhzb87@gmail.com>
2018-05-21 18:11:31 +0900

    commit

--- study/urls.py
+++ study/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-}
+    url(r'', include('blog.urls'))
+]
