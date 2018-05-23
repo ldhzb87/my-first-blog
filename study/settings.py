de6abaf8ee61313a82d93dfb668d5fb4d9d1766d
ldh <ldhzb87@gmail.com>
2018-05-23 16:06:45 +0900

    class postgresql로 교체

--- study/settings.py
+++ study/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'study.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
