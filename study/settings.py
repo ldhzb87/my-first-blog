62443943a40f4281fff9823c5aaf5b6af496aa86
ldh <ldhzb87@gmail.com>
2018-05-23 16:11:36 +0900

    class pythonanywhere에서 postgresql 안됨

--- study/settings.py
+++ study/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'study.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'heum',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
